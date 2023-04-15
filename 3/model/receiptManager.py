1439080885920f2ecb877da923aa1672dcbd38d2
Miroslav <miroslav-pankratov@mail.ru>
2023-04-15 09:17:45 +0700

    upd

---
+++ 3/model/receiptManager.py
@@ -0,0 +1,14 @@
+from typing import List
+from receipt import Receipt
+
+class ReceiptManager:
+    def __init__(self):
+        self.receipts = []
+
+    def create_receipt(self, orders, total, tax, discount, final_total):
+        receipt = Receipt(orders, total, tax, discount, final_total)
+        self.receipts.append(receipt)
+        return receipt
+
+    def get_receipts(self) -> List[Receipt]:
+        return self.receipts
