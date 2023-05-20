736f83d893c619d7db5b3fc7509e58584ba0b7eb
Miroslav <miroslav-pankratov@mail.ru>
2023-05-20 15:17:44 +0700

    !

--- 3/data/factory.py
+++ 3/data/factory.py
@@ -2,9 +2,9 @@ from data.repository import *
 from data.uow import *
 
 class WaiterSerializer:
-    def repositize(self, waiter, format):
+    def repositize(self, format):
         serializer = get_repositizer(format)
-        return serializer(waiter)
+        return serializer()
 
 def get_repositizer(format:str):
     if format.lower() == 'json':
@@ -13,13 +13,13 @@ def get_repositizer(format:str):
         return _xml_repositizer
     else: return ValueError('Invalid format')
     
-def _json_repositizer(waiter):
+def _json_repositizer():
     return JsonRepo()
     # uow = JsonUoW()
     # with uow:
     #     uow.repo.add(waiter)
 
-def _xml_repositizer(waiter):
+def _xml_repositizer():
     return XMLRepo()
     # uow = XMLUoW()
     # with uow:
